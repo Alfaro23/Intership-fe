68528433f673ac70c39924951b0d82c08ee13110
Alfaro23 <vterdohleb87@gmail.com>
2023-01-09 15:12:07 +0200

    add article list and detais article

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import './Articles.css';
+import { Articles } from './pages/Articles';
+
+import { Routes, Route } from 'react-router-dom';
+import DetailsArticle from './pages/DetailsArticle';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/article'>
+        <Route path='/article' element={<Articles/>}></Route>
+        <Route path='/article/:id' element={<DetailsArticle/>}></Route>
+      </Route>
+    </Routes>
   );
 }
 
